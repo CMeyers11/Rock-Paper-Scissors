7199dcb502c1a0ef9dede47c519c0d9a0d6107d2
Chad M <meyerschad11@gmail.com>
2025-11-04 05:27:27 -0500

    Write pseudocode for RPS exercise

---
+++ rps.js
@@ -0,0 +1,26 @@
+console.log("hello, world!")
+
+/* 
+Get choice from computer
+    Generate a random choice of R P or S
+        Use random number generator to generate a value for rand_numb
+        If rand_numb is less than or equal to 1/3 comp_choice = R, else if LTET 2/3 comp_choice = P, else comp_choice = S
+Get choice from user
+    Prompt user to enter R P or S
+    Store users entry as user_choice
+Compare choice from user vs. computer
+    If user_choice === comp_choice print "tie"
+    else if 
+        user_choice === r && comp_choice = s 
+        || 
+        user_choice = p && comp_choice = r
+        || user_choice = s && comp_choice = p
+    return "You win!"
+    else
+        return "You lose!"
+*/
+
+function getComputerChoice() {
+    
+}
+
